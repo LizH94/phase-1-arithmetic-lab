3ac0dfcc39008e3f006bc59c406887c2c7a20a2e
Elizabeth Hammond <lizh94@aol.com>
2022-06-25 13:17:54 -0400

    commit tree d619663a960e719fcf3d7689a06a6be6add1a68a

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+multiply;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+mod;
+
+
+
+let max = (2, 5, 8, 20)
+max.max();
